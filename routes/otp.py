2e168d5d474e26925099f395b4b246bf3521e3e9
ihebslimen <ihebplt1@gmail.com>
2023-04-06 06:41:47 +0100

    adding jwt access token

--- routes/otp.py
+++ routes/otp.py
@@ -6,10 +6,19 @@ import os
 from twilio.rest import Client
 import pyotp
 import datetime
+from db import mongo
+import jwt
+from bson import  json_util
+import json
+from dotenv import load_dotenv
 
+
+
+load_dotenv()
 
 
 otp_bp = Blueprint('otp', __name__)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Define OTP parameters
 OTP_LENGTH = 6
@@ -19,10 +28,9 @@ OTP_VALIDITY_TIME = 300  # seconds
 
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
-account_sid = "AC83f8ff69932eb7732bcdc8ffab17811e"
-auth_token = "4400fed24b0559d20eaf4c381ef82574"
-verify_sid = "VA7c70e19f60cbff7b003c99a726fd9fbd"
-verified_number = "+21655554907"
+account_sid = os.environ.get('TWILIO_ACCOUNT_ID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+verify_sid = os.environ.get('TWILIO_VERIFY_ID')
 
 client = Client(account_sid, auth_token)
 
@@ -42,15 +50,20 @@ def login():
     # Authenticate user
     #username = request.form['email']
     data = request.get_json()
+    res = mongo.db.users.find_one({'email': data['email'] })
+    if res is None :
+        return 'Wrong Credentials'
     
     # Store OTP secret and timestamp in session
-    session['username'] = data['email']
+    serialized_user_id = json.loads(json_util.dumps(res['_id']))
+    session['user_id'] = serialized_user_id
+    session['phone']=res['phone']
     session['otp_timestamp'] = datetime.datetime.now().timestamp()
 
     # Send OTP to user (e.g. via SMS or email)
     verification = client.verify.v2.services(verify_sid) \
       .verifications \
-      .create(to=verified_number, channel="sms")
+      .create(to=res['phone'], channel="sms")
 
     print(verification.status)
     return 'OTP generated successfully'
@@ -61,10 +74,11 @@ def login():
 def verify_otp():
     # Verify OTP
     data = request.get_json()
-    username = session.get('username')
+    user_id = session.get('user_id')
+    verified_number = session.get('phone')
     otp_timestamp = session.get('otp_timestamp')
-    print(username , otp_timestamp)
-    if not username or not otp_timestamp:
+    print(user_id , otp_timestamp)
+    if not user_id or not otp_timestamp:
         return 'Invalid session', 401
 
     # Check OTP validity
@@ -75,9 +89,11 @@ def verify_otp():
         .verification_checks \
         .create(to=verified_number, code=data['otp_code'])
     print(verification_check.status) 
+    
     # Create access token (e.g. using JWT)
-
-    return 'OTP verified successfully'
+    
+    access_token = jwt.encode({'user_id': user_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, SECRET_KEY)
+    return jsonify({'access_token': access_token})
 
 
 
