2d9f13dfd909fa99df91a37d078225b9c112fd67
ihebslimen <ihebplt1@gmail.com>
2023-05-16 12:20:15 +0100

    import secret key

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -5,10 +5,11 @@ import os
 from flask_cors import CORS
 from flask_jwt_extended import  jwt_required
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 admin_bp = Blueprint('admin', __name__)
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
 @jwt_required()
 @admin_bp.before_request
 def permissions_check():
