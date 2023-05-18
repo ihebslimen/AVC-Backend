701edc143261041fd2d7cff7e131440ba8de3ee8
ihebslimen <ihebplt1@gmail.com>
2023-05-18 10:35:54 +0100

    minor bug

--- routes/transaction_route.py
+++ routes/transaction_route.py
@@ -16,7 +16,7 @@ web3 = Web3(WebsocketProvider(ENDPOINT))
 
 contracts =  {}
 # Set the path to the folder containing the files
-folder_path = '../AVC-Blockchain/build/contracts'
+folder_path = '../AVC-Blockchain-master/build/contracts'
 
 # Loop through all the files in the folder
 for filename in os.listdir(folder_path):
