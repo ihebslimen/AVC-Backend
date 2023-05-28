4b17c4132f7fcbc8d93dd74f28afa8205566cd31
ihebslimen <ihebplt1@gmail.com>
2023-05-28 14:08:15 +0100

    login approch 2

--- routes/otp_route.py
+++ routes/otp_route.py
@@ -20,8 +20,24 @@ from models.user import User
 
 load_dotenv()
 
-OTP_VALIDITY_TIME = 300  # seconds
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# Define OTP parameters
+OTP_LENGTH = 6
+OTP_VALIDITY_TIME = 300  # seconds
+# Set the expiration time of the token to 1 hour
+expiration_time = datetime.timedelta(hours=12)
+
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+account_sid = os.environ.get('TWILIO_ACCOUNT_ID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+verify_sid = os.environ.get('TWILIO_VERIFY_ID')
+
+client = Client(account_sid, auth_token)
+
 
 
 # Authenticate a user and generate an OTP
@@ -30,38 +46,29 @@ def login():
     # Authenticate user
     #username = request.form['email']
     data = request.get_json()
-    print(data)
-    res = User.filter_users(data)
-    print(res)
-    if len(res) == 0:
+    res = mongo.db.users.find_one({'cin': data['cin'] })
+    if res is None :
         res = jsonify({"Error" : 'Wrong Credentials'})
         res.status_code = 401
         return res
+
+    elif res['role'] == 'user':
     
-    elif (res[0]['role'] == 'user'):
-        if res[0]['state'] != "approved" :
+        if res['state'] != "approved":
             res = jsonify({"Error" : 'User not approved by admin'})
             res.status_code = 401
             return res
     
     # Store OTP secret and timestamp in session
-    serialized_user_id = str(res[0]['_id'])
-    session['user_id'] = serialized_user_id
-    session['phone']=res[0]['phone']
-    session['role']=res[0]['role']
-    session['otp_timestamp'] = datetime.datetime.now().timestamp()
-    session['public_key'] = res[0]['public_key']
-    session['private_key'] = res[0]['private_key']
-
-    print(res[0]['phone'])
-
-    result = OTP.sendOTP(res[0]['phone'])
+    serialized_user_id = str(res['_id'])
+    otp_timestamp = str(datetime.datetime.now().timestamp())
+    result = OTP.sendOTP(res['phone'])
     if result != 'pending':
         res = jsonify({"Error" : result})
         res.status_code = 404
         return res
 
-    res = jsonify({"Message" : "Login OTP generated successfully"})
+    res = jsonify({"Message" : "Login OTP generated successfully", 'data': {'_id':serialized_user_id, 'otp_timestamp':otp_timestamp}})
     res.status_code = 200
     return res
 
@@ -71,25 +78,24 @@ def login():
 def login_verification():
     # Verify OTP
     data = request.get_json()
-    user_id = session.get('user_id')
-    verified_number = session.get('phone')
-    role = session.get('role')
-    otp_timestamp = session.get('otp_timestamp')
-    public_key = session.get('public_key')
-    private_key = session.get('private_key')
-    session.clear()
+    res = User.get_one_user(data['_id'])
+    user_id =data['_id']
+    verified_number = res['phone']
+    role = res['role']
+    otp_timestamp = data['otp_timestamp']
+    public_key = res['public_key']
+    print(user_id , otp_timestamp)
     if not user_id or not otp_timestamp:
         res = jsonify({"Message" :'Invalid session' })
         res.status_code = 401
         return res
 
     # Check OTP validity
-    if datetime.datetime.now().timestamp() - otp_timestamp > OTP_VALIDITY_TIME:
+    if datetime.datetime.now().timestamp() - float(otp_timestamp) > OTP_VALIDITY_TIME:
         res = jsonify({"Message" :'OTP has expired' })
         res.status_code = 401
         return res
 
-    print(verified_number , data['otp_code'])
 
     result = OTP.verifyOTP(verified_number,data['otp_code'])
 
@@ -99,7 +105,7 @@ def login_verification():
         return res
     
     # Create access token (e.g. using JWT)
-    payload = {'user_id': user_id, 'role' : role, 'public_key' : public_key , 'private_key' : private_key, 'exp': datetime.datetime(9999, 12, 31)}
+    payload = {'user_id': user_id, 'role' : role, 'public_key' : public_key ,  'exp': datetime.datetime(9999, 12, 31)}
     access_token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
     res = jsonify({"Message" :"Login successfully" , "data": access_token })
     res.status_code = 200
@@ -108,9 +114,7 @@ def login_verification():
 @shared_bp.route('/signup',  methods=['POST'])
 def signup():
     data = request.get_json()
-    session['data']=data
     result = OTP.sendOTP(data['phone'])
-
     if result != 'pending':
         res = jsonify({"Error" : result})
         res.status_code = 404
@@ -122,17 +126,14 @@ def signup():
 
 @shared_bp.route('/signup_verification', methods=['POST'])
 def signup_verification():
-    req = request.get_json()
-    data = session.get('data')
-    verified_number = data['phone']
-
-    result = OTP.verifyOTP(verified_number,req['otp_code'])
+    data = request.get_json()
+    result = OTP.verifyOTP(data['phone'],data['otp_code'])
     if result != 'approved':
         res = jsonify({"Error" : result})
         res.status_code = 404
         return res
-    print(data)
-    result1 = User.create_user(cin = data['cin'], name=data['name'], email=data['email'], phone=data['phone'],type=data['type'],actorInfoJson=data['actorInfoJson'] , role='user',state = "waiting")
+    
+    result1 = User.create_user(data['cin'], data['name'], data['email'], data['phone'], role='user',state = "waiting")
     
     if result1 :
         res = jsonify({'Message': "Signup request successful, waiting for the admin to approve it" })
