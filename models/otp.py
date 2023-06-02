2bee0a624c350745aed643faaeeed86809062172
ihebslimen <ihebplt1@gmail.com>
2023-06-02 17:02:53 +0100

    test

--- models/otp.py
+++ models/otp.py
@@ -31,9 +31,9 @@ expiration_time = datetime.timedelta(hours=12)
 
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
-account_sid = 'AC5782b21395fb63eb3edb8080ca84b22e'
-auth_token = '6b0bff9560371d0f1b2045631d9a625c'
-verify_sid = 'VAdfe95cc61a3700fa82d2088bf0be189c'
+account_sid = 'AC1b211d3ecdf084b654d505c1a59849eb'
+auth_token = 'f69430bec6f984c927dac2834a99f813'
+verify_sid = 'VAe01ed983ad46c32120141ecfcb66bc15'
 
 
 client = Client(account_sid, auth_token)
